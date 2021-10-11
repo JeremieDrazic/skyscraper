5ebc77908f643a2f5ab1861f2c0b04d207a2ea84
Jeremie Drazic <jerem.drazic@gmail.com>
2021-10-11 20:54:09 +0200

    node project setup

---
+++ packages/server/src/browser.ts
@@ -0,0 +1,20 @@
+import puppeteer from 'puppeteer'
+
+async function startBrowser() {
+  let browser = null
+
+  try {
+    console.log('opening the browser...')
+    browser = await puppeteer.launch({
+      headless: false,
+      args: ['--disable-setuid-sandbox'],
+      ignoreHTTPSErrors: true
+    })
+  } catch (error) {
+    console.error('Could not create a browser instance => : ', error)
+  }
+
+  return browser
+}
+
+export default startBrowser
